c1e96ae34e53826e9904f5b46cb2ac38e333471e
Raúl <raul.parada040103@gmail.com>
2026-05-03 01:09:47 +0200

    chore: Añadido sitemap + .npmrc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [react()],
-  adapter: vercel()
+  site: "https://orbita.devparada.dev",
+  output: "server",
+  integrations: [react(), sitemap()],
+  adapter: vercel(),
+  vite: {
+    build: {
+      chunkSizeWarningLimit: 1000,
+    },
+  },
 });
